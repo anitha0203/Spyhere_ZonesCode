21fa8582a06cf149f330665e0791e7fe219a0aa3
anitha0203 <anithab@visheshcountrycache.com>
2021-11-02 17:12:34 +0530

    https://github.com/anitha0203/Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,FormsModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,MatButtonModule,
+    AppRoutingModule,MatCardModule,MatToolbarModule,MatIconModule,MatAutocompleteModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
